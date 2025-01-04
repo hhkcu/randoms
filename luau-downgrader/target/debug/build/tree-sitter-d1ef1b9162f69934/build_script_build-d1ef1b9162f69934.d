0dd05d2da8c38cc17be8cffe1209c8201af361ca
hkcu <72319973+hhkcu@users.noreply.github.com>
2025-01-04 02:42:54 +0000

    xzf

--- luau-downgrader/target/debug/build/tree-sitter-d1ef1b9162f69934/build_script_build-d1ef1b9162f69934.d
+++
@@ -1,5 +0,0 @@
-/workspaces/randoms/luau-downgrader/target/debug/build/tree-sitter-d1ef1b9162f69934/build_script_build-d1ef1b9162f69934: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tree-sitter-0.20.10/binding_rust/build.rs
-
-/workspaces/randoms/luau-downgrader/target/debug/build/tree-sitter-d1ef1b9162f69934/build_script_build-d1ef1b9162f69934.d: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tree-sitter-0.20.10/binding_rust/build.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/tree-sitter-0.20.10/binding_rust/build.rs:
--- luau-downgrader/target/debug/deps/shlex-f85bb5f3cccdefdd.d
+++
@@ -1,8 +0,0 @@
-/workspaces/randoms/luau-downgrader/target/debug/deps/libshlex-f85bb5f3cccdefdd.rmeta: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/bytes.rs
-
-/workspaces/randoms/luau-downgrader/target/debug/deps/libshlex-f85bb5f3cccdefdd.rlib: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/bytes.rs
-
-/workspaces/randoms/luau-downgrader/target/debug/deps/shlex-f85bb5f3cccdefdd.d: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/bytes.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/lib.rs:
-/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/bytes.rs:
